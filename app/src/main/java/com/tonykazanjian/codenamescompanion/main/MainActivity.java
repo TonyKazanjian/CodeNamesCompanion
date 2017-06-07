bff70c0f9850910c47768a4d33d56544fa688f09
Tony Kazanjian <tony.kazanjian@gmail.com>
2017-06-07 11:18:19 -0700

    drawer closes

--- app/src/main/java/com/tonykazanjian/codenamescompanion/main/MainActivity.java
+++ app/src/main/java/com/tonykazanjian/codenamescompanion/main/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -112,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
             selectItem(i);
-            
+            mDrawerLayout.closeDrawer(Gravity.START);
         }
     }
 
